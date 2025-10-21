7839b5b47de05ba56a4526c68c1219b4539b72d8
Muhammad Asif <asif.pkml@gmail.com>
2025-10-21 10:46:56 -0700

    landing

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -3,7 +3,7 @@ import ZeroTrust from "@/components/landing/why-choose";
 import ZeroTenants from "@/components/landing/about-us";
 import Benefite from "@/components/landing/meet-my-team";
 // import Approach from "@/components/landing/approach";
-import GetTouch from "@/components/landing/get-touch";
+// import GetTouch from "@/components/landing/get-touch";
 import Frequently from "@/components/landing/frequently";
 const LandingPage = () => {
   return (
