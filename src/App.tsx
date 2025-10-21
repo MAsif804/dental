80a91772b7c0cd0d0138562c0f8d942e012845e6
Muhammad Asif <asif.pkml@gmail.com>
2025-10-21 10:41:44 -0700

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/ui/footer";
+// import Footer from "./components/ui/footer";
 // import Header from "./components/ui/header";
 import LandingPage from "./pages/landing";
 import Services from "./pages/services";
